78d6c1f0d018eb5c79b7739b9fe4e0f4b80b6395
coucoudu71 <technodu71@gmail.com>
2025-01-30 17:27:34 +0100

    Added function de decode ASCII numbers, to main code, context saving in comment

--- TP.asm
+++ TP.asm
@@ -1,6 +1,3 @@
-;Chiffres EN ASCII
-
-
 SECTION INTVEC
 
 B main
@@ -12,7 +9,7 @@ main
 
 ;CREE PILE
 LDR SP,=PILE1
-ADD SP, SP, #0x3c
+ADD SP, SP, #0x78
 
 
 MOV R10, #10;Juste stock 10 pour x10
@@ -22,6 +19,7 @@ LDR R1, =calc;Adresse debut calcul
 ;Convertit caractères en int et place dans PILE1 chaque int
 whileSep
 LDRB R2, [R1];Charge premier octet
+BL decodeChiffreASCII
 
 CMP R2, #0x2a;*
 BEQ executeCalc
@@ -92,38 +90,35 @@ BX LR
 ;PUSH {R0}
 ;BX LR
 
+;Fonction decode chiffres ASCII
+decodeChiffreASCII
+;PUSH {R0, R1, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14};Sauvegarde contexte
+CMP R2, #0x30
+BCS sup30
+;POP {R0, R1, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14}
+BX LR;Sors de la fonction si input < 0x30
+
+
+sup30
+CMP R2, #0x39
+BLS valeurOK
+;POP {R0, R1, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14}
+BX LR;Sors de la fonction si input > 0x39
+
+valeurOK
+SUB R2, R2, #0x30
+;POP {R0, R1, R3, R4, R5, R6, R7, R8, R9, R10, R11, R12, R13, R14}
+BX LR;Sors de la fonction après avoir convertit input
+;Fin decodeChiffreASCII
+
 fin
 
 B fin
 
 SECTION DATA
 
-PILE1 ALLOC32 15
+PILE1 ALLOC32 30
 
-calc ASSIGN8 4, 0x20, 3, 0x20, 7, 0x20, 0x2b, 0x2a, 0x20;Ajout de 0x20 pour conclure le truc ?
+;calc ASSIGN8 2, 0x20, 4, 0x20, 3, 0x20, 7, 0x20, 0x2b, 0x2a, 0x2a, 0x20;Ajout de 0x20 pour conclure le truc ?
 
-
-;Fonction decodeASCII avec R0 = input:
-
-MOV R0, #0x22
-
-BL decodeChiffreASCII;Appel de la fonction
-B fin
-
-
-;Fonction decode chiffres ASCII
-decodeChiffreASCII
-CMP R0, #0x30
-BCS sup30
-BX LR;Sors de la fonction si input < 0x30
-
-
-sup30
-CMP R0, #0x39
-BLS valeurOK
-BX LR;Sors de la fonction si input > 0x39
-
-valeurOK
-SUB R0, R0, #0x30
-BX LR;Sors de la fonction après avoir convertit input
-;Fin decodeChiffreASCII
+calc ASSIGN8 0x31, 0x30, 0x20, 0x32, 0x30, 0x20, 0x2b, 0x20
